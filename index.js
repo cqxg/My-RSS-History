f2e719b5656bd291cab218aaef7adafa12e86e80
cqxg <cqxg@yandex.ru>
2019-04-27 13:27:42 +0300

    feat: add App function and global state

---
+++ index.js
@@ -0,0 +1,9 @@
+function App() {
+    // -------------------- State -------------------------------
+    const state = {
+      currColor: 'red', // picked, red, blue
+      prevColor: 'blue', // picked, red, blue
+      clickHandler: bucketHandler // bucket, picker, mover, transformer
+    }
+
+}
