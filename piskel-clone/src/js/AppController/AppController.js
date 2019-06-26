5cf77db6b55602ce143dfffef8923054c47b0eca
cqxg <cqxg@yandex.ru>
2019-06-26 21:12:40 +0300

    feat: add pen tool

--- piskel-clone/src/js/AppController/AppController.js
+++ piskel-clone/src/js/AppController/AppController.js
@@ -3,9 +3,22 @@ import '../../css/style.css';
 
 export default class AppController {
   constructor() {
+    this.view = new AppView();
+    this.dones = ['turn', 'clone', 'mirror', 'centering', 'bucket-full', 'bucket', 'pipette', 'pen', 'line', 'eraser', 'rectngle', 'stroke-rectngle', 'circle', 'stroke-circle', 'play', 'stop', 'full', 'save', 'add', 'move'];
+    this.do = 'pen';
+    this.was = 'pen';
+    this.tools = document.querySelector('.tools');
 
-  }
-}
+    this.tools.addEventListener('click', (e) => {
+      this.done(e);
+      if (this.do === 'pen' || this.do === 'eraser' || this.do === 'line' || this.do === 'circle' || this.do === 'stroke-circle' || this.do === 'stroke-rectngle' || this.do === 'rectngle') {
+        document.getElementById('range-wrapper').style.display = 'block';
+        // eslint-disable-next-line no-shadow
+        document.getElementById('width').addEventListener('change', e => this.view.changeWidth(e));
+      } else {
+        document.getElementById('range-wrapper').style.display = 'none';
+      }
+    });
 
-const controller = new AppController();
-document.addEventListener('DOMContentLoaded', controller.init);
+    const controller = new AppController();
+    document.addEventListener('DOMContentLoaded', controller.init);
--- piskel-clone/src/js/AppView/AppView.js
+++ piskel-clone/src/js/AppView/AppView.js
@@ -2,6 +2,47 @@ import AppModel from '../AppModel/AppModel';
 
 export default class AppView {
   constructor() {
+    this.canvas_cont = document.querySelector('.canvas');
+    this.prevCanvasImg = document.getElementById('prevCanvas_img');
+    this.prevCanvas = document.getElementById('prevCanvas');
+    this.canvas = document.getElementById('myCanvas');
+    this.backgroundColor = document.getElementById('myColor');
+    this.context = this.canvas.getContext('2d');
+    this.paint = false;
+    this.model = new AppModel();
+    this.color = 'black';
+    this.backroundcolor = 'white';
+    this.width = 10;
 
   }
-}
+  down(e) {
+    this.paint = true;
+    this.context.beginPath();
+    const xCanvas = (e.pageX - this.canvas_cont.offsetLeft);
+    const yCanvas = (e.pageY - this.canvas_cont.offsetTop);
+    this.context.moveTo(xCanvas, yCanvas);
+  }
+
+  move(e, str = 'pen') {
+    if (this.paint === true) {
+      const xCanvas = e.pageX - this.canvas_cont.offsetLeft;
+      const yCanvas = e.pageY - this.canvas_cont.offsetTop;
+      this.context.lineTo(xCanvas, yCanvas);
+      this.context.lineWidth = this.width;
+      if (str === 'pen') this.context.strokeStyle = this.color;
+      else this.context.globalCompositeOperation = 'destination-out';
+      this.context.stroke();
+    }
+  }
+
+  up() {
+    this.paint = false;
+    this.context.globalCompositeOperation = 'source-over';
+  }
+
+  clear() {
+    this.backroundcolor = 'white';
+    this.backgroundColor.style.backgroundColor = this.backroundcolor;
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+};
